582e275a48b0026748764682703114364da607a9
palepoivre <pierre.antoine.lepoivre@live.fr>
2020-02-10 14:17:59 +0100

    Merge remote-tracking branch 'origin/master'

--- node_src/app.js
+++ node_src/app.js
@@ -3,6 +3,67 @@ const app = express()
 
 let connect = require("./connection.js")
 let config = require("./config.js")
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'Todolist';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('todo');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs);
+    callback(docs);
+  });
+}
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('todo');
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a : 2 }
+    , { $set: { b : 1 } }, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    console.log("Updated the document with the field a equal to 2");
+    callback(result);
+  });  
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
